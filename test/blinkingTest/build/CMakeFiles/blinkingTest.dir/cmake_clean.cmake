792da775711b4f487ff41a1c386fef3c4a3dba69
Haitem Bahri <haitem_bahri@yahoo.com>
2021-03-30 17:44:52 +0100

    builting blinkingTest

---
+++ test/blinkingTest/build/CMakeFiles/blinkingTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blinkingTest.dir/blinkingTest.cxx.o"
+  "blinkingTest.pdb"
+  "blinkingTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blinkingTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
