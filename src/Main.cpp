1736a21583327b4214d5ddf83eabfec90ad50911
Haitem Bahri <haitem_bahri@yahoo.com>
2021-03-30 17:39:50 +0100

    Adding main.cxx file

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+  cout << "**  SmartEcoPlantSystem - Realtime Embedded Programming  **\n" ;
+  cout << "**  Main Application Entry Point  **\n" ;
+  
+  
+  return 0;
+}
